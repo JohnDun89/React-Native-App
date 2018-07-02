63645e17a935de62497b39db2f7ca8a58c44f51b
John Duncan <john@johnduncanphoto.com>
2018-07-02 10:57:17 +0100

    refactor each class to seperate file

--- react-native-app/App.js
+++ react-native-app/App.js
@@ -1,61 +1,9 @@
 import React from "react";
 import DetailsScreen from "./detailsScreen";
+import ContactScreen from "./contactScreen";
+import HomeScreen from "./home.js";
 import { View, Text, Button } from "react-native";
 import { createStackNavigator } from "react-navigation";
-
-class HomeScreen extends React.Component {
-  static navigationOptions = {
-    title: "Home",
-    headerStyle: {
-      backgroundColor: "#f4511e"
-    },
-    headerTintColor: "#fff",
-    headerTitleStyle: {
-      fontWeight: "bold"
-    }
-  };
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go To Details"
-          onPress={() =>
-            this.props.navigation.navigate("Details", { name: "John" })
-          }
-        />
-      </View>
-    );
-  }
-}
-
-class ContactScreen extends React.Component {
-  static navigationOptions = {
-    title: "Contact",
-    headerStyle: {
-      backgroundColor: "#f4511e"
-    },
-    headerTintColor: "#fff",
-    headerTitleStyle: {
-      fontWeight: "bold"
-    }
-  };
-  render() {
-    const { navigation } = this.props;
-    const name = navigation.getParam("name", "No Name");
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Contact Screen</Text>
-        <Text> 0789 66 00398</Text>
-
-        <Button
-          title="Go back"
-          onPress={() => this.props.navigation.navigate("Homegit")}
-        />
-      </View>
-    );
-  }
-}
 
 const RootStack = createStackNavigator(
   {
--- react-native-app/contactScreen.js
+++ react-native-app/contactScreen.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
 import { createStackNavigator } from "react-navigation";
-import ContactScreen from "./contactScreen.js";
 
-class DetailsScreen extends React.Component {
+class ContactScreen extends React.Component {
   static navigationOptions = {
-    title: "Details",
+    title: "Contact",
     headerStyle: {
       backgroundColor: "#f4511e"
     },
@@ -19,19 +18,16 @@ class DetailsScreen extends React.Component {
     const name = navigation.getParam("name", "No Name");
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Details Screen</Text>
-        <Text> User Name: {JSON.stringify(name)}</Text>
+        <Text>Contact Screen</Text>
+        <Text> 0789 66 00398</Text>
+
         <Button
           title="Go back"
-          onPress={() => this.props.navigation.goBack()}
-        />
-        <Button
-          title="Contact"
-          onPress={() => this.props.navigation.navigate("Contact")}
+          onPress={() => this.props.navigation.navigate("Home")}
         />
       </View>
     );
   }
 }
 
-export default DetailsScreen;
+export default ContactScreen;
