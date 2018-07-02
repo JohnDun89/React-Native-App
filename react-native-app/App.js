f77254db0978565ca603eafc883c6896b885ccbf
John Duncan <john@johnduncanphoto.com>
2018-07-02 10:44:30 +0100

    refactor to seperate file

--- react-native-app/App.js
+++ react-native-app/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import DetailsScreen from "./detailsScreen";
 import { View, Text, Button } from "react-native";
 import { createStackNavigator } from "react-navigation";
 
@@ -28,37 +29,6 @@ class HomeScreen extends React.Component {
   }
 }
 
-class DetailsScreen extends React.Component {
-  static navigationOptions = {
-    title: "Details",
-    headerStyle: {
-      backgroundColor: "#f4511e"
-    },
-    headerTintColor: "#fff",
-    headerTitleStyle: {
-      fontWeight: "bold"
-    }
-  };
-  render() {
-    const { navigation } = this.props;
-    const name = navigation.getParam("name", "No Name");
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Details Screen</Text>
-        <Text> User Name: {JSON.stringify(name)}</Text>
-        <Button
-          title="Go back"
-          onPress={() => this.props.navigation.goBack()}
-        />
-        <Button
-          title="Contact"
-          onPress={() => this.props.navigation.navigate("Contact")}
-        />
-      </View>
-    );
-  }
-}
-
 class ContactScreen extends React.Component {
   static navigationOptions = {
     title: "Contact",
@@ -80,7 +50,7 @@ class ContactScreen extends React.Component {
 
         <Button
           title="Go back"
-          onPress={() => this.props.navigation.navigate("Home")}
+          onPress={() => this.props.navigation.navigate("Homegit")}
         />
       </View>
     );
---
+++ react-native-app/detailsScreen.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { View, Text, Button } from "react-native";
+import { createStackNavigator } from "react-navigation";
+
+class DetailsScreen extends React.Component {
+  static navigationOptions = {
+    title: "Details",
+    headerStyle: {
+      backgroundColor: "#f4511e"
+    },
+    headerTintColor: "#fff",
+    headerTitleStyle: {
+      fontWeight: "bold"
+    }
+  };
+  render() {
+    const { navigation } = this.props;
+    const name = navigation.getParam("name", "No Name");
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Details Screen</Text>
+        <Text> User Name: {JSON.stringify(name)}</Text>
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
+        <Button
+          title="Contact"
+          onPress={() => this.props.navigation.navigate("Contact")}
+        />
+      </View>
+    );
+  }
+}
+
+export default DetailsScreen;
