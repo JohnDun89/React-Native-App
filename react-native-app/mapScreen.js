d0f72c46d792d6885a55be202ada60fac771c2bc
John Duncan <john@johnduncanphoto.com>
2018-07-16 12:46:13 +0100

    prop types added to map screen

--- react-native-app/mapScreen.js
+++ react-native-app/mapScreen.js
@@ -1,16 +1,23 @@
 import React from "react";
 import MapContainer from "./mapContainer.js";
+import PropTypes from "prop-types";
 
 class MapScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+
   render() {
     return (
       <MapContainer lng={this.props.longitude} lat={this.props.latitude} />
     );
   }
 }
+
+MapScreen.propTypes = {
+  lng: PropTypes.number,
+  lat: PropTypes.number
+};
 
 export default MapScreen;
